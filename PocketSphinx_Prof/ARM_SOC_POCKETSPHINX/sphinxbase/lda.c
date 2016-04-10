7123b06d5a9dccd2354d00642e35b0c3fee42de5
Adrizcorp <Adrizcorp@gmail.com>
2016-04-10 16:41:55 -0500

    Pocket Sphinx ARM SOC

--- PocketSphinx_Prof/ARM_SOC_POCKETSPHINX/sphinxbase/lda.c
+++ PocketSphinx_Prof/ARM_SOC_POCKETSPHINX/sphinxbase/lda.c
@@ -42,9 +42,9 @@
 
 #include <assert.h>
 #include <string.h>
-#ifdef HAVE_CONFIG_H
-#include <config.h>
-#endif
+//#ifdef HAVE_CONFIG_H
+#include "config.h"
+//#endif
 
 #ifdef _MSC_VER
 #pragma warning (disable: 4018)
